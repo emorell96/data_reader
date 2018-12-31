55efe70aa7b124bd5ac98e686d9bf84a34e70069
Enrique Morell <emorell96@hotmail.com>
2018-12-31 15:40:26 +0100

    added imports

--- data_reader/__init__.py
+++ data_reader/__init__.py
@@ -1,3 +1,5 @@
 #definition of global constants (all written in capital letters)
 #sets how much text is displayed when executing functions from the package
 VERBOSE_LVL = 0
+import data_reader.logic
+import data_reader.helpers
